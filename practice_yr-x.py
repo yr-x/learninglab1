8e886c5c7db2ae8398a63c3bf65e811c6f5e0e3e
yr <yrlee0913@gmail.com>
2022-03-03 15:15:35 +0900

    설정과 매뉴얼 포함

--- practice_yr-x.py
+++ practice_yr-x.py
@@ -1,8 +1,12 @@
+# 초기 설정을 구현합니다  
 # Git Bash 실행 
     # git config --global user.name 이름
     # git config --global user.email 이메일 주소
 # VSCode 실행
-    # git init
+    # New File 생성 
+    # 파일명은 practice_닉네임.py 
+    # 적당한 폴더에 저장 <- 앞으로 변경하지 않습니다 
+    # git init 
     # git add 파일명 또는 git add .
     # git commit -m "제출합니다"
     # git remote add origin https://github.com/yr-x/learninglab1.git
@@ -10,6 +14,6 @@
     # git push -u origin main 
 
 
-# 시작하겠습니다 
-print("hello world")
-print(3+5)
+# 학습을 시작합니다 
+print("hello world") #hello world
+print(3+5) #8
