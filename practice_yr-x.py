f3435ef3aaf361e6b4520d888394784f1e9f278d
yr <yrlee0913@gmail.com>
2022-03-03 13:27:16 +0900

    초기 설정 포함

--- practice_yr-x.py
+++ practice_yr-x.py
@@ -1,2 +1,15 @@
+# Git Bash 실행 
+    # git config --global user.name 이름
+    # git config --global user.email 이메일 주소
+# VSCode 실행
+    # git init
+    # git add 파일명 또는 git add .
+    # git commit -m "제출합니다"
+    # git remote add origin https://github.com/yr-x/learninglab1.git
+    # git branch -M main 
+    # push -u origin main 
+
+
+# 시작하겠습니다 
 print("hello world")
 print(3+5)
