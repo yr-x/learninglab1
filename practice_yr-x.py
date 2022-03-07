74f3428dd70488656511f9821d1781ade4199ebe
yr <yrlee0913@gmail.com>
2022-03-07 10:37:39 +0900

    test

--- practice_yr-x.py
+++ practice_yr-x.py
@@ -7,9 +7,10 @@
     # 파일명은 practice_닉네임.py 
     # 적당한 폴더에 저장 <- 앞으로 변경하지 않습니다 
     # git init 
+    # git remote add origin https://github.com/yr-x/learninglab1.git
     # git add 파일명 또는 git add .
     # git commit -m "제출합니다"
-    # git remote add origin https://github.com/yr-x/learninglab1.git
+ 
     # git branch -M main 
     # git push -u origin main 
 
