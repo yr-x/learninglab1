24a6e6d0b031617ab55fbeef7c926f4e0b1b02a1
yr <yrlee0913@gmail.com>
2022-03-08 08:54:43 +0900

    test

--- practice_manual.py
+++ practice_manual.py
@@ -16,5 +16,38 @@
 
 # 학습을 시작합니다 
 print("hello world") #hello world
-print(3+5) #8
 
+print(2 + 3 * 4) #14
+print((2 + 3) * 4) #20
+number = 2 + 3 * 4 #14
+print(number)
+number = number + 2 #16
+print(number)
+number += 2 #18
+print(number)
+number *= 2 #36
+print(number)
+number /=2 #18
+print(number)
+number -= 2 #16
+print(number)
+number %= 5 #1 나머지
+print(number)
+
+print(abs(-5)) #5 절대값
+print(pow(4, 2)) #4^2 제곱
+print(max(5, 12)) #12 최대값
+print(min(5, 12)) #5 최소값 
+print(round(3.14)) #3 반올림 
+
+
+animal = "고양이"
+name = "해피"
+age = 4
+hobby = "낮잠"
+is_adult = age >= 3
+
+print("우리집" + animal + "의 이름은" + name + "예요")
+hobby = "공놀이"
+print(name,"는 ", age, "살이며", hobby, "을 아주 좋아해요")
+print(name+ "는 어른일까요?" + str(is_adult))
