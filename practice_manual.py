2eba02031a71f1a0749441c9090dc07c22383911
yr <yrlee0913@gmail.com>
2022-03-11 12:33:44 +0900

    commit

--- practice_manual.py
+++ practice_manual.py
@@ -53,6 +53,7 @@ print(min(5, 12)) #5 최소값
 print(round(3.14)) #3 반올림 
 print(round(4.99)) #5
 
+from ipaddress import NetmaskValueError
 from math import *
 print(floor(4.99)) #내림 4
 print(ceil(3.14)) #올림 4
@@ -126,3 +127,75 @@ print("나는 {}색과 {}색을 좋아해요".format("파란", "빨간"))
 print("나는 {0}색과 {1}색을 좋아해요".format("파란", "빨간"))
 print("나는 {1}색과 {0}색을 좋아해요".format("파란", "빨간"))
 print("나는 {age}살이며, {color}색을 좋아햏요".format(age = 20, color = "빨간"))
+
+age = 20 
+color = "빨간"
+print(f"나는 {age}살이며, {color}색을 좋아해요")
+
+# 탈출문
+print("백문이 불여일견 \n백견이 불여일타") # \n 줄바꿈
+print('저는 "나도코딩"입니다.')
+print("저는 \"나도코딩\"입니다.") # \" \' 문장 내에서 따옴표
+print ("C:\Users\2920513\Hyundai Motor and Kia\My Document\PythonWorkbook\practice_manual.py") # \\문장 내에서 \
+print("Red Apple\rPine") # \r 커서를 맨앞으로 이동 
+print("Redd\bApple") # \b 백스페이스 한글자 삭제 
+print("Red\tApple") # \t 탭
+
+# # Q 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오
+# 예) http://naver.com
+# 규칙1 http:// 부분은 제외 => naver.com
+# 규칙2 처음 만나는 점(.) 이후 부분은 제외 => Naver
+# 규칙3 남은 글자 중 처음 세자리 + 글자갯수 + 글자 내 'e' 갯수 + "!"로 구성
+# 예) 생성 비밀번호 nav51!
+url = "http://naver.com"
+my_str = url.replace("http://", "") # 규칙1
+print(my_str)
+my_str = my_str[:my_str.index(".")] # my_str[0:5] -> 0~5 직전까지 0,1,2,3,4
+print(my_str)
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print("{0}의 비밀번호는 {1}입니다.".format(url, password))
+
+# 리스트 []
+# 지하철 칸별로 10명, 20명, 30명
+subway1 = 10
+subway2 = 20
+subway3 = 30
+subway = [10, 20, 30]
+print(subway)
+
+subway = ["유재석", "조세호", "박명수"]
+# 조세호씨가 몇 번째 칸에 타고 있는가
+print(subway.index("조세호")) # 1
+#하하씨가 다음 정류장에서 다음 칸에 탐
+subway.append("하하")
+print(subway)
+# 정형돈씨를 유재석 / 조세호 사이에 태워봄
+subway.insert(1, "정형돈")
+#지하철에 있는 사람을 한명씩 뒤에서 꺼냄
+print(subway.pop())
+print(subway.pop())
+print(subway.pop())
+print(subway)
+# 같은 이름의 사람이 몇 명 있는지 확인
+subway.append("유재석")
+print(subway.count("유재석"))
+# 정렬도 가능
+num_list = [5,2,4,3,1]
+num_list.sort()
+print(num_list)
+# 순서 뒤집기 가능
+num_list.reverse()
+print(num_list)
+#모두 지우기
+num_list.clear()
+print(num_list)
+# 다양한 자료형 함께 사용
+mix_list = ["조세호", 20, True]
+print(mix_list)
+# 리스트 확장
+num_list.extend(mix_list)
+print(num_list)
+
+# 사전
+
+
