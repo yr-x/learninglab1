60d443056cd476fe3e82d1e78b7135e174f5c9be
yr <yrlee0913@gmail.com>
2022-03-17 13:00:32 +0900

    test

--- practice_manual.py
+++ practice_manual.py
@@ -1,4 +1,4 @@
-# 초기 설정을 구현합니다  
+# 환경 설정 
 # Git Bash 실행 
     # git config --global user.name 이름
     # git config --global user.email 이메일 주소
@@ -14,9 +14,10 @@
     # git push -u origin main 
 
 
-# 학습을 시작합니다 
+# 학습시작 
 print("hello world") #hello world
 
+# 연산자 
 print(2 + 3 * 4) #14
 print((2 + 3) * 4) #20
 number = 2 + 3 * 4 #14
@@ -34,6 +35,7 @@ print(number)
 number %= 5 #1 나머지
 print(number)
 
+# 변수
 animal = "고양이"
 name = "해피"
 age = 4
@@ -45,7 +47,7 @@ hobby = "공놀이"
 print(name,"는 ", age, "살이며", hobby, "을 아주 좋아해요")
 print(name+ "는 어른일까요?" + str(is_adult))
 
-
+# 숫자처리함수 
 print(abs(-5)) #5 절대값
 print(pow(4, 2)) #4^2 제곱
 print(max(5, 12)) #12 최대값
@@ -59,6 +61,7 @@ print(floor(4.99)) #내림 4
 print(ceil(3.14)) #올림 4
 print(sqrt(16)) #제곱근 4
 
+# 랜덤함수 
 from random import *
 print(random()) #0~1 임의값
 print(random() * 10) # 0~10
@@ -136,7 +139,7 @@ print(f"나는 {age}살이며, {color}색을 좋아해요")
 print("백문이 불여일견 \n백견이 불여일타") # \n 줄바꿈
 print('저는 "나도코딩"입니다.')
 print("저는 \"나도코딩\"입니다.") # \" \' 문장 내에서 따옴표
-print ("C:\Users\2920513\Hyundai Motor and Kia\My Document\PythonWorkbook\practice_manual.py") # \\문장 내에서 \
+print ("C:\\Users\\2920513\\Hyundai Motor and Kia\\My Document\\PythonWorkbook\\practice_manual.py") # \\문장 내에서 \
 print("Red Apple\rPine") # \r 커서를 맨앞으로 이동 
 print("Redd\bApple") # \b 백스페이스 한글자 삭제 
 print("Red\tApple") # \t 탭
@@ -186,7 +189,7 @@ print(num_list)
 # 순서 뒤집기 가능
 num_list.reverse()
 print(num_list)
-#모두 지우기
+# 모두 지우기
 num_list.clear()
 print(num_list)
 # 다양한 자료형 함께 사용
@@ -197,5 +200,62 @@ num_list.extend(mix_list)
 print(num_list)
 
 # 사전
-# 
+cabinet = {3:"유재석", 100:"김태호"}
+print(cabinet[3])
+print(cabinet[100])
+print(cabinet.get(3))
+# print(cabinet[5]) # 값 없음 프로그램 오류 종료
+print(cabinet.get(5)) # 값 없음 None 출력 프로그램 지속
+print(cabinet.get(5, "사용 가능"))
+print("hi")
 
+print(3 in cabinet) #true
+print(5 in cabinet) #false
+
+cabinet = {"A-3":"유재석", "B-100":"김태호"}
+print(cabinet["A-3"])
+# 새 손님
+print(cabinet)
+cabinet["A-3"] = "김종국"
+cabinet["C-20"] = "조세호"
+print(cabinet)
+
+# 떠난 손님
+del cabinet["A-3"]
+# key, value 출력
+print(cabinet.keys())
+print(cabinet.values())
+print(cabinet.items())
+cabinet.clear()
+
+# 튜플 (변경되지 않는 목록, 리스트보다 빠름)
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+# menu.add("생선까스") 함수 이용 불가 
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
+
+# 세트 (중복 안됨, 순서없음)
+my_set = {1,2,3,3,3}
+print(my_set)
+java = {"유재석", "김태호", "양세형"}
+python = set(["유재석", "박명수"])
+# 교집합 (java와 python을 모두 할 수 있는 개발자)
+print(java & python)
+print(java.intersection(python))
+# 합집합 (java하거나 python 할 수 있는 개발자)
+print(java | python)
+print(java.union(python))
+# 차집합 (java할 수 있지만 python 할줄 모르는 개발자)
+print(java - python)
+print(java.difference(python))
+# python 할줄 아는 사람이 늘어남
+python.add("김태호")
+print(python)
+# java를 잊어버림
+java.remove("김태호")
+print(java)
