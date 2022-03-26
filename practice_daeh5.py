bd6159dc993ffa43ea96a832b74eb9607a7035f4
daeh5 <dhdeankim@gmail.com>
2022-03-26 12:50:03 +0900

    제출

---
+++ practice_daeh5.py
@@ -0,0 +1,11 @@
+from json.tool import main
+from mimetypes import init
+from operator import getitem
+
+
+git init
+git remote add origin https://github.com/yr-x/learninglab1.getitem
+git add practice_daeh5.py
+git commit -m "제출합니다"
+git push origin main
+git pull origin main
