371d06dc9c736e41c1d218325a3a0329bcda3d1f
yr <yrlee0913@gmail.com>
2022-04-05 12:56:53 +0900

    스타크래프트 전까

--- practice_manual.py
+++ practice_manual.py
@@ -594,6 +594,58 @@ for line in lines:
 score_file.close()
 
 
-# 피클 
+# pickle 
+import pickle
+profile_file = open("profile.pickle", "wb") # 피클은 바이너리 타입 정의 반드시, 인코딩 필요없음
+profile = {"이름":"박명수", "나이":30, "취미":["축구","골프","코딩"]}
+print(profile)
+pickle.dump(profile, profile_file)
+profile_file.close() 
 
+profile_file = open("profile.pickle", "rb")
+profile = pickle.load(profile_file) # file에 있는 정보를 profile에 불러오기 
+profile(profile)
+profile_file.close()
+
+# with 
+import pickle 
+
+with open("profile.pickle", "rb") as profile_file:
+    print(pickle.load(profile_file))
+
+with open("study.txt", "w", encoding="utf8") as study_file:
+    study_file.write("파이썬을 열심히 공부하고 있어요")
+
+with open("study.txt", "r", encoding="utf8") as study_file:
+    print(study_file.read())
+
+# Q. 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다.
+# 보고서는 항상 아래와 같은 형태로 출력되어야 합니다.
+# -  x 주차 주간보고 - 
+# 부서 :
+# 이름 :
+# 업무 요약: 
+
+# 1주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오
+# 조건: 파일명은 '1주차. txt', '2주차. txt', ...와 같이 만듭니다. 
+
+for i in range(1, 51):
+    with open(str(i)+ "주차.txt", "w", encoding="utf8") as report_file:
+        report_file.write("- {0} 주차 주간보고".format(i))
+        report_file.write("\n부서 :")
+        report_file.write("\n이름 :")
+        report_file.write("\n업무 요약: ")
+
+# 클래스 (스타크래프트)
+# 마린 : 공격유닛, 군인, 총을 쏠 수 있음
+name = "마린" 
+hp = 40
+damage = 5 
+print("{} 유닛이 생성되었습니다".format(name))
+print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+
+# 탱크 ; 공격유닛, 탱크, 포를 쏠 수 있고 일반/시즈 모드
+tank_name = "탱크"
+tank_hp = 150
+tank_damage = 35
 
