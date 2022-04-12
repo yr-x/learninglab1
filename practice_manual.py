e87891733eb0746843f21531e34365019531b415
yr <yrlee0913@gmail.com>
2022-04-12 16:41:23 +0900

    스타크래프트 후반전

--- practice_manual.py
+++ practice_manual.py
@@ -1,7 +1,7 @@
 # 환경 설정 
 # Git Bash 실행 
-    # git config --global user.name 이름
-    # git config --global user.email 이메일 주소
+    # git config --global user.name "이름"
+    # git config --global user.email "이메일 주소"
 # VSCode 실행
     # New File 생성 
     # 파일명은 practice_닉네임.py 
@@ -591,7 +591,7 @@ while True:
 score_file.close()
 
 score_file = open("score.txt", "r", encoding="utf8")
-lines = score_file.realines()
+lines = score_file.readlines()
 for line in lines:
     print(line, end="")
 score_file.close()
@@ -607,7 +607,7 @@ profile_file.close()
 
 profile_file = open("profile.pickle", "rb")
 profile = pickle.load(profile_file) # file에 있는 정보를 profile에 불러오기 
-profile(profile)
+print(profile)
 profile_file.close()
 
 # with 
@@ -640,8 +640,10 @@ for i in range(1, 51):
         report_file.write("\n업무 요약: ")
 
 
-# 클래스 (스타크래프트)
 
+
+# 클래스 (스타크래프트)
+from random import * 
 # 마린 : 공격유닛, 군인, 총을 쏠 수 있음
 name = "마린" 
 hp = 40
@@ -650,7 +652,218 @@ print("{} 유닛이 생성되었습니다".format(name))
 print("체력 {0}, 공격력 {1}\n".format(hp, damage))
 
 # 탱크 ; 공격유닛, 탱크, 포를 쏠 수 있고 일반/시즈 모드
-tank_name = "탱크"
-tank_hp = 150
-tank_damage = 35
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
 
+# print("{} 유닛이 생성되었습니다".format(tank_name))
+# print("체력 {0}, 공격력 {1}\n".format(hp, tank_damage))
+
+# def attack(name, location, damage):
+#     print("{0} : {1} 방향으로 적군을 공격합ㄴ다. [공격력 {2}".래금ㅅ( \
+#         name, location, damage))
+
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+
+# 일반유닛 
+class Unit: # class 복사, 객체 생성 self 제외하고 모두 넣어주어야 
+    def __init__(self, name, hp, speed):  # __ 생성자 
+        self.name = name
+        self.hp = hp
+        self.speed = speed
+        print("{0}유닛이 생성되었습니다".format(name))
+
+    def move(self, location):
+        print("[지상유닛 이동]")
+        print("{0}:{1}방향으로 이동합니다 [속도 {2}"\
+            .format(self.name, location, self.speed))
+
+    def damaged(self, damage):
+        print("{0}:{1} 데미지를 입었습니다".format(self.name, damage))
+        self.hp -= damage
+        print("{0}:현재 체력은 {1}입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0}: 파괴되었습니다.".format(self.name))
+
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
+
+# # 레이스 : 공중유닛, 비행기, 클로킹(스탤스)
+# wraith1 = Unit("레이스", 80, 5)
+# print("유닛이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage)) # .으로 
+
+# # 마인드컨트롤 : 상대방 유닛을 내 것으로 만드는것 
+# wraith2 = Unit("레이스", 80, 5)
+# wraith2.clocking = True # clocking 외부 변수 추가 할당
+
+# if wraith2.clocking == True:
+#     print("{0}는 현재 클로킹 상태입니다".format(wraith2.name))
+
+# 공격유닛 
+class AttackUnit(Unit): # 일반유닛 부모클래스 Unit 상속, 성격 물려받음  
+        def __init__(self, name, hp, speed, damage):  # __ 생성자 
+            Unit.__init__(self, name, hp, speed) # name과 hp 상속 
+            self.damage = damage # damage만 추가 
+
+        def attack(self, location):
+            print("{0}:{1} 방향으로 적군을 공격합니다. [공격력 {2}"\
+                .format(self.name, location, self.damage))
+
+# 마린
+class Marine(AttackUnit):
+    def __init__(self):
+        AttackUnit.__init__(self, "마린", 40, 1, 5)
+
+    # 스팀팩: 일정 시간 공격 이동/속도 증가, 자기체력 10 감소 
+    def stimpack(self):
+        if self.hp > 10:
+            self.hp -= 10
+            print("{0}: 스팀팩을 사용합니다. (HP 10감소)".format(self.name))
+        else:
+            print("{0}: 체력이 부족하여 스팀팩을 사용하지 않습니다.".format(self.name))
+
+# 탱크
+class Tank(AttackUnit):
+    # 시즈모드: 탱크를 지상에 고정, 더 높은 파워 공격, 이동불가
+    seize_developed = False # 시즈모드 개바ㅏㄹ여부
+    def __init__(self):
+        AttackUnit.__init__(self, "탱크", 150, 1, 35)
+        self.seize_mode = False 
+   
+    def set_seize_mode(self):
+        if Tank.seize_developed == False:
+            return
+    # 시즈모드가 아닐 때 -> 시즈모드 
+        if self.seize_mode == False:
+            print("{0}: 시즈모드로 전환합니다".format(self.name))
+            self.damage *= 2 
+            self.seize_mode = True 
+
+    # 시즈모드일때 -> 해제  
+        else:
+            print("{0}: 시즈모드를 해제합니다".format(self.name))
+            self.damage /= 2 
+            self.seize_mode = False 
+
+# 파이어뱃: 공격유닛, 화염방사기
+firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+
+# 공격 2번 받는다고 가정
+firebat1.damaged(25)
+firebat1.damaged(25)
+
+# 드랍쉽 : 공중유닛, 수송기, 공격기능 없음 
+class Flyable: # 날수 있는 기능 가진 클래스  
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed 
+
+    def fly(self, name, location):
+        print("{0}:{1} 방향으로 날아갑니다. [속도 {2}"\
+            .format(name, location, self.flying_speed))
+
+# 공중공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable): # 다중상속, 클래스 두개 상속 
+    def __init__(self, name, hp, damage, flying_speed): 
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상스피드는 0 
+        Flyable.__init__(self, flying_speed)
+
+    def move(self, location):  # 메소드 오버라이딩 
+        self.fly(self.name, location) 
+
+# 발키리: 공중공격유닛, 한번에 14발 미사일 발사 
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
+
+
+# 벌처 : 지상유닛, 기동성이 좋음
+vulture = AttackUnit("벌처", 80, 10, 20)
+# 배틀크루저 : 공중유닛, 체력좋음, 공격력 좋음
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+battlecruiser.move("9시") 
+
+
+# pass
+# 건물
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, location):
+       # Unit.__init__(self, name, hp, 0)
+        super().__init__(name, hp, 0) # self 없음 
+        self.location = location 
+
+# 레이스
+class Wraith(FlyableAttackUnit): 
+    def __init__(self):
+        FlyableAttackUnit.__init__(self, "레이스", 80, 20, 5)
+        self.clocked = False # 클로킹 해제 상태
+    
+    def clocking(self):
+        if self.clocked == True: # 클로킹모드 -> 해제 
+            print("{0}: 클로킹모드 해제 합니다".format(self.name))
+            self.clocked = False
+        else:    # 클로킹 해제 -> 모드 설정        
+            print("{0}: 클로킹모드 설정 합니다".format(self.name))
+            self.clocked = True 
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다")
+def game_over():
+    print("Player : GG")
+    print("[Player]님이 게임에서 퇴장하셨습니다")
+
+
+# 게임 시작 
+game_start()
+
+# 마린 3기 생성 
+m1 = Marine()
+m2 = Marine()
+m3 = Marine()
+
+# 탱크 2기 생성
+t1 = Tank()
+t2 = Tank()
+
+# 레이스 1기 ㅅ애성
+w1 = Wraith()
+
+# 유닛 일괄 관리
+attack_units = []
+attack_units.append(m1)
+attack_units.append(m2)
+attack_units.append(m3)
+attack_units.append(t1)
+attack_units.append(t2)
+attack_units.append(w1)
+
+# 전군 이동
+for unit in attack_units:
+    unit.move("1시")
+
+# 탱크 시즈모드 개발
+Tank.seize_developed = True
+print("[알림] 탱크 시즈 모드 개발이 완료되었습니다")
+
+# 공격 모드 준비 (마린: 스팀팩, 탱크: 시즈모드, 레이스: 클로킹)
+for unit in attack_units:
+    if isinstance(unit, Marine): # ~의 경우 
+        unit.stimpack()
+    elif isinstance(unit, Tank):
+        unit.set_seize_mode()
+    elif isinstance(unit, Wraith):
+        unit.clocking()
+
+# 전군 공격 
+for unit in attack_units:
+    unit.attack("1시")
+
+# 전군 피해
+for unit in attack_units:
+    unit.damaged(randint(5, 21)) # 공격 랜덤으로 받음 5~20 
+
+# 게임 종료
+game_over() 
