566d72ef907af9528e35809af98d8785e88a9c49
yr <yrlee0913@gmail.com>
2022-04-15 09:48:43 +0900

    함수 완료

--- practice_manual.py
+++ practice_manual.py
@@ -643,13 +643,14 @@ for i in range(1, 51):
 
 
 # 클래스 (스타크래프트)
+
 from random import * 
 # 마린 : 공격유닛, 군인, 총을 쏠 수 있음
-name = "마린" 
-hp = 40
-damage = 5 
-print("{} 유닛이 생성되었습니다".format(name))
-print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+# name = "마린" 
+# hp = 40
+# damage = 5 
+# print("{} 유닛이 생성되었습니다".format(name))
+# print("체력 {0}, 공격력 {1}\n".format(hp, damage))
 
 # 탱크 ; 공격유닛, 탱크, 포를 쏠 수 있고 일반/시즈 모드
 # tank_name = "탱크"
@@ -676,7 +677,7 @@ class Unit: # class 복사, 객체 생성 self 제외하고 모두 넣어주어�
 
     def move(self, location):
         print("[지상유닛 이동]")
-        print("{0}:{1}방향으로 이동합니다 [속도 {2}"\
+        print("{0}:{1}방향으로 이동합니다 [속도 {2}]"\
             .format(self.name, location, self.speed))
 
     def damaged(self, damage):
@@ -703,13 +704,13 @@ class Unit: # class 복사, 객체 생성 self 제외하고 모두 넣어주어�
 
 # 공격유닛 
 class AttackUnit(Unit): # 일반유닛 부모클래스 Unit 상속, 성격 물려받음  
-        def __init__(self, name, hp, speed, damage):  # __ 생성자 
-            Unit.__init__(self, name, hp, speed) # name과 hp 상속 
-            self.damage = damage # damage만 추가 
+    def __init__(self, name, hp, speed, damage):  # __ 생성자 
+        Unit.__init__(self, name, hp, speed) # name과 hp 상속 
+        self.damage = damage # damage만 추가 
 
-        def attack(self, location):
-            print("{0}:{1} 방향으로 적군을 공격합니다. [공격력 {2}"\
-                .format(self.name, location, self.damage))
+    def attack(self, location):
+        print("{0}:{1} 방향으로 적군을 공격합니다. [공격력 {2}]"\
+            .format(self.name, location, self.damage))
 
 # 마린
 class Marine(AttackUnit):
@@ -727,7 +728,7 @@ class Marine(AttackUnit):
 # 탱크
 class Tank(AttackUnit):
     # 시즈모드: 탱크를 지상에 고정, 더 높은 파워 공격, 이동불가
-    seize_developed = False # 시즈모드 개바ㅏㄹ여부
+    seize_developed = False # 시즈모드 개발 여부
     def __init__(self):
         AttackUnit.__init__(self, "탱크", 150, 1, 35)
         self.seize_mode = False 
@@ -748,7 +749,7 @@ class Tank(AttackUnit):
             self.seize_mode = False 
 
 # 파이어뱃: 공격유닛, 화염방사기
-firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1 = AttackUnit("파이어뱃", 50, 16, 10)
 firebat1.attack("5시")
 
 # 공격 2번 받는다고 가정
@@ -828,7 +829,7 @@ m3 = Marine()
 t1 = Tank()
 t2 = Tank()
 
-# 레이스 1기 ㅅ애성
+# 레이스 1기 생성 
 w1 = Wraith()
 
 # 유닛 일괄 관리
@@ -867,3 +868,168 @@ for unit in attack_units:
 
 # 게임 종료
 game_over() 
+
+
+# Q8. 주어진 코드를 활용하여 부동산 프로그램을 작성하시오.
+# (출력 예제)
+# 총 3대의 매물이 있습니다.
+# 강남 아파트 매매 10억 2010년
+# 마포 오피스텔 전세 5억 2007년
+# 송파 빌라 월세 500/50 2000년 
+
+class House:
+    # 매물 초기화
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price 
+        self.completion_year = completion_year 
+
+    # 매물 정보 표시
+    def show_detail(self):
+        print(self.location, self.house_type, self.deal_type, self.price, self.completion_year)
+
+houses = []
+house1 = House("강남", "아파트", "매매", "10억", "2010년")
+house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+house3 = House("송파", "빌라", "월세", "500/50", "2000년")
+houses.append(house1)
+houses.append(house3)
+houses.append(house3)
+
+print("총 {0}개의 매물이 있습니다.".format(len(houses)))
+for house in houses:
+    house.show_detail()
+
+
+# 예외처리
+try: 
+    print("나누기 전용 계산기 입니다")
+    nums = []
+    nums.append(int(input("첫번째 숫자를 입력하세요 : ")))
+    nums.append(int(input("두번째 숫자를 입력하세요 : ")))
+    # nums.append(int(nums[0] / nums[1])) # 오류 발생 시키기 
+    print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
+except ValueError:
+    print("에러! 잘못된 값을 입력하였습니다") 
+except ZeroDivisionError as err:
+    print(err) 
+except: 
+    print("알 수 없는 에러가 발생하였습니다") 
+
+# 에러 발생시키기, 사용자정의 예외처리, finally (무조건실행)
+class BigNimberError(Exception):
+    def __init__(self, msg):
+        self.msg = msg 
+
+    def __str__(self):
+        return self.msg 
+
+try: 
+    print("한자리 숫자 나누기 전용 계산기입니다")
+    num1 = int(input("첫번째 숫자를 입력하세요 : "))
+    num2 = int(input("두번째 숫자를 입력하세요 : ")) 
+    if num1 >= 10 or num2 >= 10:
+        raise BigNimberError("입력값: {0}, {1}".format(num1, num2))
+    print("{0} / {1} = {2}".forat(num1, num2, int(num / num2)))
+except ValueError: 
+    print("잘못된 값을 입력하였습니다. 한자리 숫자만 입력하세요") 
+except BigNimberError as err:
+    print("에러가 발생하였습니다. 한자리 숫자만 입력하세요") 
+    print(err)
+finally: 
+    print("계산기를 이용해주셔서 감사합니다")
+
+
+# Q9. 동네에 항상 대기 손님이 있는 치킨집이 있습니다.
+# 대기 손님의 치킨 요리 시간을 줄이고자 자동 주문 시스템을 제작합니다
+# 시스템 코드를 확인하고 적절한 예외처리 구문을 넣으시오
+
+# 조건1: 1보다 작거나 숫자가 아닌 입력값이 들어올때는 ValueError 처리
+#       출력메시지: 잘못된 값을 입력하였습니다
+# 조건2: 대기 손님이 주문할수 있는 총 치킨양은 10마리 한정
+#       치킨 소진 시 사용자 정의 에러(SoldOutError) 발생시키고 프로그램 종료
+#       출력메시지: 재고가 소진되어 더이상 주문을 받지 않습니다
+
+class SoldOutError(Exception):
+    pass 
+chicken = 10
+waiting = 1 # 홀은 현재 만석, 대기번호 1부터 시작
+while(True):
+    try: 
+        print("[남은치킨 : {0}]".format(chicken))
+        order = int(input("치킨 몇마리 주문하시겠습니까"))
+        if order > chicken: # 남은 치킨보다 주문량이 많음
+            print("재료가 부족합니다")
+        elif order <= 0:
+            raise ValueError 
+        else:
+            print("[대기번호 {0}] {1}마리 주문이 완료되었습니다".format(waiting, order))
+            waiting += 1
+            chicken -= order 
+
+        if chicken == 0:
+            raise SoldOutError 
+    except ValueError:
+        print("잘못된 값을 입력하였습니다")
+    except SoldOutError:
+        print("재고가 소진되어 더이상 주문을 받지 않습니다")
+        break
+
+
+# 모듈 
+import theater_model
+theater_model.price(3) # 3명 영화 가격
+theater_model.price_morning(4) # 조조 
+theater_model.price_soldier(5) # 군인
+
+import theater_model as mv # 모듈 별명 
+mv.price(3)
+
+from theater_model import * 
+price(3)
+price_morning(4)
+price_soldier(5)
+
+from theater_model import price, price_morning 
+price(5)
+price_morning(3)
+
+from theater_model import price_soldier as price 
+price(5)
+
+# 패키지 
+import travel.thailand # 클래스 import 안 됨 
+trip_to = travel.thailand.ThailandPackage()
+trip_to.detail()
+
+from travel.thailand import ThailandPackage # 클래스 import 가능 
+trip_to = ThailandPackage()
+trip_to.detail
+
+from travel import vietnam
+trip_to = vietnam.VietnmaPackage()
+trip_to.detail()
+
+
+# __all__
+from travel import *
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
+
+
+# Q10. 프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드시오
+# 조건: 모듈 파일명은 byme.py 
+# (모듈 사용 예제)
+# import byme
+# byme.sign()
+
+# (출력 예제)
+# 이 프로글매은 나도코딩에 의해 만들어졌습니다
+# 유튜브 http://youtube.com
+# 이메일 nadocoding@gmail.com
+
+import byme
+byme.sign() 
+
